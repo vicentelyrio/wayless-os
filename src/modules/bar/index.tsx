6cf243b53a337a4bd591b26f637184a0c4384610
vicentelyrio <vicentelyrio@gmail.com>
2025-07-31 16:55:41 -0300

    feat(v2): using css tokens

--- src/modules/bar/index.tsx
+++ src/modules/bar/index.tsx
@@ -7,10 +7,8 @@ import { Bar } from './Bar'
 import tokens from '@styles/tokens.css'
 import style from './styles.css'
 
-app.apply_css(tokens)
-
 app.start({
-  css: style,
+  css: tokens.concat(style),
   instanceName: 'WaylessOS-Bar',
   main() {
     app.get_monitors().map(Bar)
